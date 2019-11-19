0e4b0a92607cf183b6cbbaedeb7f9a30f6a6f9eb
蒋家焘 <15304072722@163.com>
2019-11-19 20:02:45 +0800

    注释语句

---
+++ JJTLogin/Controller/JJTController.h
@@ -0,0 +1,19 @@
+//
+//  JJTController.h
+//  JJTLogin
+//
+//  Created by softium on 2019/11/19.
+//  Copyright © 2019 softium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JJTController : NSObject
+
+-(void)print;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JJTLogin/Controller/JJTController.m
@@ -0,0 +1,17 @@
+//
+//  JJTController.m
+//  JJTLogin
+//
+//  Created by softium on 2019/11/19.
+//  Copyright © 2019 softium. All rights reserved.
+//
+
+#import "JJTController.h"
+
+@implementation JJTController
+
+-(void)print{
+    NSLog(@"%@",@"123445566677");
+}
+
+@end
